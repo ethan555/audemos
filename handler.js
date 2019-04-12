356ada1ecd651150ea21fbd736893d0d9bf8f578
Ethan Williams <ethan555williams@gmail.com>
2019-04-12 15:04:39 -0500

    Added index.html, updated yml to fit s3 uploading

--- handler.js
+++ handler.js
@@ -1,14 +1,22 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+// module.exports.hello = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: 'Go Serverless v1.0! Your function executed successfully!',
+//       input: event,
+//     }),
+//   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
+
+module.exports.postprocess = (event) => {
+  event.Records.forEach((record) => {
+    const filename = record.s3.object.key;
+    const filesize = record.s3.object.size;
+    console.log(`New .mp3 object has been created: ${filename} (${filesize} bytes)`);
+  });
 };
