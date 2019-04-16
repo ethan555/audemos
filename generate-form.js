a244c701c6084baa0d871038c60e5870d650ef20
Ethan Williams <ethan555williams@gmail.com>
2019-04-16 18:29:04 -0500

    Messing around with stuff

---
+++ generate-form.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const crypto = require('crypto');
+const fs = require('fs');
+
+const awsAccessKeyId = 'AKIAJY7JXXFGPOKIU34A';
+const awsSecretAccessKey = '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'
+const bucketName = 'audemos-dev-demos'
--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+
 // module.exports.hello = async (event, context) => {
 //   return {
 //     statusCode: 200,
