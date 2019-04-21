b833485d569350968f491fe17277a4ecf8fe98d8
Ethan Williams <ethan555williams@gmail.com>
2019-04-20 21:12:08 -0500

    Successfully uploads files

--- handler.js
+++ handler.js
@@ -6,17 +6,14 @@ module.exports.getURL = (event, context, callback) => {
   var s3 = new AWS.S3();
   var parameters = JSON.parse(event.body);
 
-  // Generate randomized string for the object key
-  // var random_string = "";
-  // var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  // for (var i = 0; i < 32; i ++) {
-  //   random_string += chars.charAt(Math.floor(Math.random() * chars.length));
-  // }
+  // Get the timestamp
+  var timestamp = Date.now();
+  var key = "" + timestamp + parameters.name;
 
   // What bucket to send to
   var s3Parameters = {
-    Bucket: '${S3_AUDIO_BUCKET}',
-    Key: parameters.name,
+    Bucket: process.env.S3_AUDIO_BUCKET,
+    Key: key,
     ContentType: parameters.type,
     ACL: 'public-read',
   };
@@ -30,28 +27,10 @@ module.exports.getURL = (event, context, callback) => {
         'Access-Control-Allow-Methods' : 'POST',
         'Access-Control-Allow-Headers' : 'Content-Type',
       },
-      body: JSON.stringify({ uploadURL : uploadURL }),
+      body: JSON.stringify({
+        key : key,
+        uploadURL : uploadURL
+      }),
     }
   );
 };
-
-// module.exports.hello = async (event, context) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: 'Go Serverless v1.0! Your function executed successfully!',
-//       input: event,
-//     }),
-//   };
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
-
-// module.exports.postprocess = (event) => {
-//   event.Records.forEach((record) => {
-//     const filename = record.s3.object.key;
-//     const filesize = record.s3.object.size;
-//     console.log(`New .mp3 object has been created: ${filename} (${filesize} bytes)`);
-//   });
-// };
