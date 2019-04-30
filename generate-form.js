4ef67346cac2708ff8cb76c8cb1642a1ba8cf2ad
Ethan Williams <ethan555williams@gmail.com>
2019-04-30 15:07:19 -0500

    Trimming

--- generate-form.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const crypto = require('crypto');
-const fs = require('fs');
-
-const awsAccessKeyId = 'AKIAJY7JXXFGPOKIU34A';
-const awsSecretAccessKey = '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'
-const bucketName = 'audemos-dev-demos'
